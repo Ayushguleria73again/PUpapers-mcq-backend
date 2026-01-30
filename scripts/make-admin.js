cbf477e6878af85c1b90e2c3946d16d07f93f157
ayushguleria73 <ayushguleria73@gmail.com>
2026-01-30 08:33:05 +0530

    Add make-admin script and update README

--- scripts/make-admin.js
+++ scripts/make-admin.js
@@ -4,37 +4,34 @@ const User = require('../models/User');
 
 dotenv.config({ path: './.env' });
 
-const email = process.argv[2];
-
-if (!email) {
-  console.error('Please provide an email address as an argument.');
-  process.exit(1);
-}
-
 const makeAdmin = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI;
-    if (!mongoUri) {
-        throw new Error('MONGODB_URI is not defined in .env');
-    }
-    
+    const mongoUri = process.env.MONGODB_URI; 
+    // Fixed: Use correct env var name MONGODB_URI from .env
+    if (!mongoUri) throw new Error('MONGODB_URI is not defined in .env');
+
     await mongoose.connect(mongoUri);
-    console.log('MongoDB Connected...');
+    console.log('Connected to DB');
+
+    const email = process.argv[2];
+    if (!email) {
+      console.log('Please provide an email: node scripts/make-admin.js <email>');
+      process.exit(1);
+    }
 
     const user = await User.findOne({ email });
-
     if (!user) {
-      console.error(`User with email ${email} not found.`);
+      console.log('User not found');
       process.exit(1);
     }
 
     user.role = 'admin';
     await user.save();
-
-    console.log(`Success! User ${user.email} is now an ADMIN.`);
+    console.log(`Success! ${user.fullName} (${user.email}) is now an Admin.`);
     process.exit(0);
+
   } catch (err) {
-    console.error('Error:', err);
+    console.error(err);
     process.exit(1);
   }
 };
